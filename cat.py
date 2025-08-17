e1f4ea742cf099e89d70fe253a63d100d3ff8d70
bumi kim <gimbumi25@gmai.com>
2025-08-17 11:52:10 +0900

    0817 bullet final for real

--- cat.py
+++ cat.py
@@ -19,19 +19,14 @@ class Cat:
         self.current_image = self.image_front
         original_rect = self.current_image.get_rect(center=(x, y)) # 고양이 대신 사각형
         new_width = int(size[0] * 0.8)  # 원래 너비의 80%
-        new_height = int(size[1] * 0.8)  
+        new_height = int(size[1] * 0.8)
         # 새로운 사각형을 만들고, 중심을 원래 위치로 설정합니다.
         self.rect = pygame.Rect(0, 0, new_width, new_height)
         self.rect.center = original_rect.center
         self.speed = 200 # 픽셀/초 단위
 
-        # 총알 방향 설정
-        self.bullets = [] #총알 리스트
-        self.last_shot_time = pygame.time.get_ticks()
-        self.direction = [0, -1] 
+        self.direction = [0, -1]  
 
-
-    
     def handle_input(self, keys, dt):
         dx = 0
         dy = 0
@@ -56,15 +51,7 @@ class Cat:
         self.rect.x += dx
         self.rect.y += dy
 
-
-    def shoot(self): #총알 쏘기
-        current_time = pygame.time.get_ticks()
-        if current_time - self.last_shot_time > 500:
-            # 총알을 생성할 때 현재 방향(self.direction)을 함께 전달합니다.
-            new_bullet = Bullet(self.rect.centerx, self.rect.centery, self.direction)
-            self.bullets.append(new_bullet)
-            self.last_shot_time = current_time
-
+    
 
     def draw(self, screen):
         screen.blit(self.current_image, self.rect) #고양이 이미지를 네모 위치에 도장찍기
